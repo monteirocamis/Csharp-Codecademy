f9e114023e08d7ef87da71e3fd5f2f553748fdfe
Camis Monteiro <monteiro.camis@gmail.com>
2021-02-24 00:21:39 -0300

    Create StringConcatenation.cs

---
+++ StringConcatenation.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace StoryTime
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Declare the variables
+    string beginning = " beginning ";
+    string middle = " middle ";
+  string end = " end " ;
+
+      // Concatenate the string and the variables
+  string story = beginning + middle + end;
+
+      // Print the story to the console 
+Console.WriteLine( story );
+    }
+  }
+}
