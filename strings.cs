fc08670b651beb70ed46422c11855542bb3f90ba
Camis Monteiro <monteiro.camis@gmail.com>
2021-02-24 00:17:03 -0300

    Create strings.cs

---
+++ strings.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace PrideAndPrejudice
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // First string variable
+  string  firstSentence = " It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife. ";
+      // Second string variable
+string firstSpeech  = " \"My dear Mr. Bennet,\" said his lady to him one day, \"have you heard that Netherfield Park is let at last?\"";
+
+      // Print variable and newline
+  Console.WriteLine(firstSentence );
+  Console.WriteLine("\n");
+   Console.WriteLine(firstSpeech );
+    }
+  }
+}
