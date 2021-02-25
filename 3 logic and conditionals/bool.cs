f1b02ba56b374e68e7a2565b58f44a92ce2afa32
Camis Monteiro <monteiro.camis@gmail.com>
2021-02-25 20:58:02 -0300

    Create bool.cs

---
+++ 3 logic and conditionals/bool.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace BooleanDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+Console.Write( "True or False: The number 500 is greater than 24?")
+
+  bool answerOne = true;
+
+Console.Write( "True or False: “coffee” contains the letter “a”.?")
+  bool answerTwo  = false;
+
+        }
+    }
+}
