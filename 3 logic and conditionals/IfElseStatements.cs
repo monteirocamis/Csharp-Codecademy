5cc9e17509fd31b844ade04d3ff44fef74d32986
Camis Monteiro <monteiro.camis@gmail.com>
2021-02-25 23:10:08 -0300

    Create IfElseStatements.cs

---
+++ 3 logic and conditionals/IfElseStatements.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace IfElseStatement
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int people = 12;
+      string weather = "bad";
+if(people <=10 && weather == "nice" ){
+Console.WriteLine("SaladMart");
+}
+  else
+  {
+    Console.WriteLine("Soup N Sandwich");
+  }
+    }
+  }
+}
