490fd6970065dcea39e032a2ce8f97f6948dafb6
Camis Monteiro <monteiro.camis@gmail.com>
2021-02-25 23:03:57 -0300

    Create IfStatements.cs

---
+++ 3 logic and conditionals/IfStatements.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace IfStatement
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int socks = 6;
+if (socks <=3) {
+Console.WriteLine("Time to do laundry!");
+}
+
+    }
+  }
+}
