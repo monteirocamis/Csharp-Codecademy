519b6a17a3b498c55ad0823e8c211533bf91edc9
Camis Monteiro <monteiro.camis@gmail.com>
2021-02-25 22:18:44 -0300

    Update ComparisonOperators.cs

--- 3 logic and conditionals/ComparisonOperators.cs
+++ 3 logic and conditionals/ComparisonOperators.cs
@@ -1,4 +1,4 @@
-Comparison operators include:
+/* Comparison operators include:
 
     Equals ==: returns true if the value to the left is equal to the value to the right.
     Inequality operator !=: returns true if the two values are not equal.
@@ -6,7 +6,7 @@ Comparison operators include:
     Greater than >: returns true if the value to the left is more than the value to the right.
     Less than or equal to <=: returns true if the value to the left is less than or equal to the value on the right.
     Greater than or equal to >=: returns true if the value to the left is more than or equal to the value to the right.
-
+*/
 
 using System;
 
