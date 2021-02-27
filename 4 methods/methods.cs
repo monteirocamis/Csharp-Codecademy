4f8f83f390bf34db65fe85a43d03914b7c995a2c
Camis Monteiro <monteiro.camis@gmail.com>
2021-02-27 00:34:15 -0300

    Update methods.cs

--- 4 methods/methods.cs
+++ 4 methods/methods.cs
@@ -15,3 +15,38 @@ namespace CallAMethod
     }
   }
 }
+//review
+using System;
+
+namespace IntroMethodOutput
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Call a method with multiple arguments
+      NamePets("Laika", "Albert");
+      // Call a method with named arguments
+      VisitPlanets(numberOfPlanets: 8);
+    }
+    
+    // Define a method with multiple parameters
+    static void NamePets(string pet1, string pet2)
+    {
+      Console.WriteLine($"Your pets {pet1} and {pet2} will be joining your voyage across space!");
+    }  
+ 
+    // Define a method with optional parameters
+    static void VisitPlanets(
+      string adjective = "brave",
+      string name = "Cosmonaut", 
+      int numberOfPlanets = 0,
+      double gForce = 4.2)
+    {
+      Console.WriteLine($"Welcome back, {adjective} {name}.");
+      Console.WriteLine($"You visited {numberOfPlanets} new planets...");
+      Console.WriteLine($"...while experiencing a g-force of {gForce} g!");
+    }
+    
+  }
+}
