1a55605998684c535f6c5f33087f74f0aaca9a91
Camis Monteiro <monteiro.camis@gmail.com>
2021-02-27 00:31:26 -0300

    Update overloadMethod.cs

--- 4 methods/overloadMethod.cs
+++ 4 methods/overloadMethod.cs
@@ -28,3 +28,48 @@ NamePets();
 
   }
 }
+
+
+//review
+
+using System;
+
+namespace ReviewMethodCallsAndInput
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+NamePets("xanel", "capuccina");
+VisitPlanets(numberOfPlanets : 2);
+    }
+    
+    static void NamePets()
+    {
+      Console.WriteLine("Aw, you have no spacefaring pets :(");
+    }
+    
+    static void NamePets(string pet1, string pet2)
+    {
+      Console.WriteLine($"Your pets {pet1} and {pet2} will be joining your voyage across space!");
+    }  
+    
+    static void NamePets(string pet1, string pet2, string pet3)
+    {
+      Console.WriteLine($"Your pets {pet1}, {pet2}, and {pet3} will be joining your voyage across space!");
+    }  
+    
+    static void VisitPlanets(
+      string adjective = "brave",
+      string name = "Cosmonaut", 
+      int numberOfPlanets = 0,
+      double gForce = 4.2)
+    {
+      Console.WriteLine($"Welcome back, {adjective} {name}.");
+      Console.WriteLine($"You visited {numberOfPlanets} new planets...");
+      Console.WriteLine($"...while experiencing a g-force of {gForce} g!");
+    }
+    
+  }
+}
+
