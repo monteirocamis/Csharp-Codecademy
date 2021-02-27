4a4b4df83cf351022453349bfe094893618fe26b
Camis Monteiro <monteiro.camis@gmail.com>
2021-02-26 23:44:51 -0300

    Create DefineMethod.cs

---
+++ 4 methods/DefineMethod.cs
@@ -0,0 +1,19 @@
+;*static void YourMethodName()
+{
+}
+*;
+using System;
+
+namespace DefineAMethod
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      VisitPlanets();
+   }
+    static void VisitPlanets(){
+      Console.WriteLine("You visited many new planets…");
+    }
+  }
+}
