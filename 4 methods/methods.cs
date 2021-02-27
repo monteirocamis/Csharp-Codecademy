309e3e9679959e29ea7d3b4f65d1ddb4cfbbc1b2
Camis Monteiro <monteiro.camis@gmail.com>
2021-02-26 23:34:22 -0300

    Create methods.cs

---
+++ 4 methods/methods.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CallAMethod
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string msg = "Yabba dabba doo!";
+      Math.Min(2,4);
+      Console.WriteLine(msg);
+     msg.Substring(0,1);
+     
+
+    }
+  }
+}
