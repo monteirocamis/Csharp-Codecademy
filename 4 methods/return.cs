dbe2662ef70828e1c4031761cb4a30d910e0e957
Camis Monteiro <monteiro.camis@gmail.com>
2021-03-03 21:40:34 -0300

    Update return.cs

--- 4 methods/return.cs
+++ 4 methods/return.cs
@@ -9,7 +9,7 @@ public static void Main()
   Console.WriteLine(output); // Prints WHO'S THERE?
 }
 
-//exercicio:
+//exercicio return:
 using System;
 
 namespace Return
@@ -22,6 +22,40 @@ namespace Return
     }
      static string DecoratePlanet(string planet) {
       return ($"*.*.* Welcome to {planet} *.*.*");
+    }
+	}
+}
+//exercicio return errors:
+
+using System;
+
+namespace ReturnErrors
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(DecoratePlanet("Mars")); 
+      Console.WriteLine("Is Pluto really a dwarf...?");
+      Console.WriteLine(IsPlutoADwarf());
+      Console.WriteLine("Then how many planets are there in the galaxy...?");
+      Console.WriteLine(CountThePlanets());
+    }
+    
+    static string DecoratePlanet(string planet)
+    {
+       return $"*..*..* Welcome to {planet} *..*..*";
+    }
+    
+    static bool IsPlutoADwarf()
+    {
+      bool answer = true;
+      return answer ;
+    }
+    
+    static string CountThePlanets()
+    {
+      return "8 planets, usually";
     }
 	}
 }
