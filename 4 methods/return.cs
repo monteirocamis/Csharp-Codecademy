8084765142b3b52c59ad84e67e1639c11c144e4c
Camis Monteiro <monteiro.camis@gmail.com>
2021-03-03 21:31:43 -0300

    Update return.cs

--- 4 methods/return.cs
+++ 4 methods/return.cs
@@ -8,3 +8,20 @@ public static void Main()
   string output = Yell("who's there?");
   Console.WriteLine(output); // Prints WHO'S THERE?
 }
+
+//exercicio:
+using System;
+
+namespace Return
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    Console.WriteLine( DecoratePlanet("Jupiter"))  ;
+    }
+     static string DecoratePlanet(string planet) {
+      return ($"*.*.* Welcome to {planet} *.*.*");
+    }
+	}
+}
