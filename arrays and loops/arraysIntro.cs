a42f452c06dfe360507d80b71dfd2b31b24a84ec
Camis Monteiro <monteiro.camis@gmail.com>
2021-03-04 01:52:21 -0300

    Create arraysIntro.cs

---
+++ arrays and loops/arraysIntro.cs
@@ -0,0 +1,33 @@
+/*
+ // examples: int[] plantHeights = { 3, 4, 6 }; OR  int[] plantHeights = new int[] { 3, 4, 6 };
+
+// Initial declaration
+int[] plantHeights;
+ 
+// This works
+plantHeights = new int[] { 3, 4, 6 };   
+ 
+// This will cause an error
+// plantHeights = { 3, 4, 6 };
+ */
+ 
+ // exercise:
+ using System;
+
+namespace BuildingArrays
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string [] summerStrut ;
+     summerStrut = new string[] {"Despacito", "Rebel Rebel" , "Juice", "Missing U","Raspberry Beret","New York Groove","Make Me Feel","Los Angeles"};
+int [] ratings = { 1,2,3,4,5,1,2,3  };
+    }
+  }
+}
+
+
+
+
+
