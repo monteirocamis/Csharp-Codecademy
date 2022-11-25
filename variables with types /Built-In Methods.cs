34466ef351adf1134a075a1e5fe5869e1ffba285
Camis <Camis@MacBook-Pro.local>
2022-11-24 23:48:25 -0300

    add: variables classes and money maker end

---
+++ variables with types /Built-In Methods.cs
@@ -0,0 +1,6 @@
+// Math.Abs()—will find the absolute value of a number. Example: Math.Abs(-5) returns 5.
+// Math.Sqrt()—will find the square root of a number. Example: Math.Sqrt(16) returns 4.
+// Math.Floor()—will round the given double or decimal down to the nearest whole number. Example: Math.Floor(8.65) returns 8.
+// Math.Min()—returns the smaller of two numbers. Example: Math.Min(39, 12) returns 12.
+
+
---
+++ variables with types /Operator Shortcuts.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace MakingProgress
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // declare steps variable
+  int steps = 0;
+      // Two steps forward 
+  steps+=2;
+
+
+      // One step back 
+
+steps-=1;
+      // Print result to the console
+Console.WriteLine(steps);
+    }
+  }
+}
