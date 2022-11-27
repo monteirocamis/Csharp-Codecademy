249f1a4a3aa7f46be61e18be934dd35fc6f0d302
Camis <Camis@MacBook-Pro.local>
2022-11-27 16:21:30 -0300

    :bug: bugfix

---
+++ variables with types /variables.cs
@@ -0,0 +1,14 @@
+   // Create Variables
+  string name  = "Shadow";
+   string breed  = "Golden Retriever";
+   int age  = 5;
+    double weight  = 65.22;
+  bool spayed = true;
+
+
+      // Print variables to the console
+Console.WriteLine(name);
+Console.WriteLine(breed);
+Console.WriteLine(age);
+Console.WriteLine(weight);
+Console.WriteLine(spayed);
